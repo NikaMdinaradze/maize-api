d48ced7e5f35fd180e658fc0583835c623dc906f
NikaMdinaradze <mdinaradzenika04@gmail.com>
2024-04-10 00:34:49 +0400

    Ref: use tortoise pydantic contrib instead of basemodels

--- src/models/user.py
+++ src/models/user.py
@@ -1,12 +1,19 @@
-from tortoise import fields
-from tortoise.models import Model
+from tortoise import fields, models
+from tortoise.contrib.pydantic import pydantic_model_creator
 
 
-class User(Model):
+class User(models.Model):
     id = fields.UUIDField(pk=True)
-    username = fields.CharField(max_length=50)
-    email = fields.CharField(max_length=100)
-    age = fields.IntField()
-    first_name = fields.CharField(max_length=50)
-    last_name = fields.CharField(max_length=50)
-    password = fields.CharField(max_length=255)
+    username = fields.CharField(max_length=20, unique=True)
+    first_name = fields.CharField(max_length=50, null=True)
+    last_name = fields.CharField(max_length=50, null=True)
+    password_hash = fields.CharField(max_length=128, null=True)
+    created_at = fields.DatetimeField(auto_now_add=True)
+    modified_at = fields.DatetimeField(auto_now=True)
+
+    class PydanticMeta:
+        exclude = ["created_at", "modified_at"]
+
+
+UserCreate = pydantic_model_creator(User, name="User")
+UserBase = pydantic_model_creator(User, name="UserIn", exclude_readonly=True)
--- src/schemas/__init__.py
+++
@@ -1,4 +0,0 @@
-from pydantic import BaseModel
-from user import UserBase, UserCreate, UserUpdate, User
-
-__all__: list[BaseModel] = [UserBase, UserCreate, UserUpdate, User]
