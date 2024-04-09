4324f73cebec107668a89b86e66908f61edc0429
NikaMdinaradze <mdinaradzenika04@gmail.com>
2024-04-09 19:15:22 +0400

    Fix: models registration

--- src/main.py
+++ src/main.py
@@ -2,18 +2,19 @@ from fastapi import FastAPI
 from tortoise.contrib.fastapi import register_tortoise
 
 from src.db import db_url
-from src.settings import DATABASE_MODELS
 
 app = FastAPI()
+
 
 @app.get("/")
 def root():
     return {"message": "root url"}
 
+
 register_tortoise(
     app,
     db_url=db_url,
-    modules={"models": DATABASE_MODELS},
+    modules={"models": ["src.models"]},
     generate_schemas=True,
     add_exception_handlers=True,
 )
