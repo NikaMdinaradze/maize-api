ffeb3780d7b9f286c8a612bc8be1dcab16ab07f6
NikaMdinaradze <mdinaradzenika04@gmail.com>
2024-04-10 13:35:33 +0400

    Chore: make essential fields not nullable

--- src/models/user.py
+++ src/models/user.py
@@ -4,10 +4,11 @@ from tortoise.contrib.pydantic import pydantic_model_creator
 
 class User(models.Model):
     id = fields.UUIDField(pk=True)
-    username = fields.CharField(max_length=20, unique=True)
-    first_name = fields.CharField(max_length=50, null=True)
-    last_name = fields.CharField(max_length=50, null=True)
-    password = fields.CharField(max_length=128, null=True)
+    username = fields.CharField(max_length=20)
+    first_name = fields.CharField(max_length=50)
+    last_name = fields.CharField(max_length=50)
+    email = fields.CharField(max_length=50)
+    password = fields.CharField(max_length=128)
     birth_date = fields.DateField()
     created_at = fields.DatetimeField(auto_now_add=True)
     modified_at = fields.DatetimeField(auto_now=True)
