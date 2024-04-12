4ecbb5de458b5cef3d9bacb27188768f7171cfa7
NikaMdinaradze <mdinaradzenika04@gmail.com>
2024-04-12 14:06:19 +0400

    Feat: add user me endpoint

--- src/endpoints/user.py
+++ src/endpoints/user.py
@@ -1,9 +1,10 @@
 from typing import List
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from pydantic import UUID4, BaseModel
 from starlette.exceptions import HTTPException
 
+from src.deps import get_current_user
 from src.models import User, UserCreate, UserView
 
 router = APIRouter(prefix="/user", tags=["user"])
@@ -22,6 +23,11 @@ async def get_users():
 async def create_user(user: UserCreate):
     user_obj = await User.create(**user.model_dump(exclude_unset=True))
     return await UserView.from_tortoise_orm(user_obj)
+
+
+@router.get("/me", response_model=UserView)
+async def user_me(user: User = Depends(get_current_user)):
+    return user
 
 
 @router.get("/{id}", response_model=UserView)
