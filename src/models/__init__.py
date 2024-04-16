92f177582ae238e5673904330101a4bef9195669
NikaMdinaradze <mdinaradzenika04@gmail.com>
2024-04-16 12:04:56 +0400

    Chore: separate models and shcemas

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,4 +1,13 @@
+from typing import Type, Any
+
 from tortoise.models import Model
+from pydantic import BaseModel
+
 from src.models.user import User, UserView, UserCreate
 
-__all__: list[Model] = [User, UserView, UserCreate]
+tortoise_models: list[Type[Model]] = [User,]
+
+schemas: list [Type[BaseModel]] = [UserView, UserCreate]
+
+__all__: list[Any] = tortoise_models + schemas
+
