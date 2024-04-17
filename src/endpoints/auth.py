d94091f26a6cb35cb78d2f139980112d504149f0
NikaMdinaradze <mdinaradzenika04@gmail.com>
2024-04-17 15:57:13 +0400

    Feat: add refresh token endpoint

--- src/endpoints/auth.py
+++ src/endpoints/auth.py
@@ -2,10 +2,12 @@ from fastapi import APIRouter, Depends, status
 from fastapi.exceptions import HTTPException
 from fastapi.security import OAuth2PasswordRequestForm
 
+from src.deps import verify_refresh_token
 from src.JWT import JWTToken
-from src.models.user import User, UserCreate, UserView
+from src.models import TokenPayload, User, UserCreate, UserView
 
 router = APIRouter(prefix="/auth", tags=["auth"])
+
 
 @router.post("/register", response_model=UserView)
 async def register(user: UserCreate):
@@ -14,6 +16,7 @@ async def register(user: UserCreate):
     """
     user_obj = await User.create(**user.model_dump(exclude_unset=True))
     return await UserView.from_tortoise_orm(user_obj)
+
 
 @router.post("/login")
 async def login(request: OAuth2PasswordRequestForm = Depends()):
@@ -35,3 +38,10 @@ async def login(request: OAuth2PasswordRequestForm = Depends()):
         "refresh_token": token.refresh_token,
         "token_type": "bearer",
     }
+
+
+@router.post("/refresh")
+def refresh_access_token(refresh_token: TokenPayload = Depends(verify_refresh_token)):
+    token = JWTToken(refresh_token.user_id)
+    access_token = token.access_token
+    return {"access_token": access_token}
