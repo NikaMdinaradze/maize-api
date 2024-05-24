90d3c57f71cd72a9419968411ace594a1ae71be2
NikaMdinaradze <mdinaradzenika04@gmail.com>
2024-05-24 16:06:37 +0400

    Feat: add unique constraints to users model

--- src/models/user.py
+++ src/models/user.py
@@ -4,10 +4,10 @@ from tortoise.contrib.pydantic import pydantic_model_creator
 
 class User(models.Model):
     id = fields.UUIDField(pk=True)
-    username = fields.CharField(max_length=20)
+    username = fields.CharField(max_length=20, unique=True)
     first_name = fields.CharField(max_length=50)
     last_name = fields.CharField(max_length=50)
-    email = fields.CharField(max_length=50)
+    email = fields.CharField(max_length=50, unique=True)
     password = fields.CharField(max_length=128)
     birth_date = fields.DateField()
     is_active = fields.BooleanField(default=False)
