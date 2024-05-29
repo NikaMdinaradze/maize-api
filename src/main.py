d77931817ac967189a7e5245ed55abe7d7d804e6
NikaMdinaradze <mdinaradzenika04@gmail.com>
2024-05-29 19:19:31 +0400

    docs: add docstrings to main.py

--- src/main.py
+++ src/main.py
@@ -3,16 +3,16 @@ from fastapi import FastAPI
 from src.endpoints import auth
 from src.settings import init_db
 
-app = FastAPI()
+app = FastAPI(title="Maize API", description="MVP api for maize", version="0.01")
 
 app.include_router(auth.router)
 
 
 @app.get("/")
-def root():
+def root() -> dict:
     return {"message": "root url"}
 
 
 @app.on_event("startup")
-async def on_startup():
+async def on_startup() -> None:
     await init_db()
