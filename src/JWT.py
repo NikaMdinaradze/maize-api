c4699536a04264f95a4c1e9400347501f4fff13b
NikaMdinaradze <mdinaradzenika04@gmail.com>
2024-05-29 19:22:20 +0400

    docs: add docstrings to JWT.py

--- src/JWT.py
+++ src/JWT.py
@@ -13,10 +13,38 @@ from src.settings import (
 
 
 class JWTToken:
+    """
+    Utility class for generating JWT tokens.
+
+    Attributes:
+        user_id (str): The user ID used to generate tokens.
+
+    Methods:
+        access_token: Property method to generate an access token.
+        refresh_token: Property method to generate a refresh token.
+        one_time_token: Property method to generate a one-time token.
+    """
+
     def __init__(self, user_id: UUID4) -> None:
+        """
+        Initialize the JWTToken object with user id.
+
+        Args:
+            user_id (UUID4): The user ID used to generate tokens.
+        """
         self.user_id = str(user_id)
 
     def _create_jwt_token(self, expires_delta: timedelta, token_type: str):
+        """
+        Create a JWT token.
+
+        Args:
+            expires_delta (timedelta): The expiration time delta for the token.
+            token_type (str): The type of token to generate.
+
+        Returns:
+            str: The generated JWT token.
+        """
         expire = datetime.now(timezone.utc) + expires_delta
         to_encode = {"user_id": self.user_id, "exp": expire, "token_type": token_type}
         encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
@@ -24,6 +52,12 @@ class JWTToken:
 
     @property
     def access_token(self):
+        """
+        Generate an access token.
+
+        Returns:
+            str: The generated access token.
+        """
         token = self._create_jwt_token(
             expires_delta=ACCESS_TOKEN_EXPIRATION, token_type="access"
         )
@@ -31,6 +65,12 @@ class JWTToken:
 
     @property
     def refresh_token(self):
+        """
+        Generate a refresh token.
+
+        Returns:
+            str: The generated refresh token.
+        """
         token = self._create_jwt_token(
             expires_delta=REFRESH_TOKEN_EXPIRATION, token_type="refresh"
         )
@@ -38,6 +78,12 @@ class JWTToken:
 
     @property
     def one_time_token(self):
+        """
+        Generate a one-time token.
+
+        Returns:
+            str: The generated one-time token.
+        """
         token = self._create_jwt_token(
             expires_delta=ONE_TIME_TOKEN_EXPIRATION, token_type="one-time"
         )
