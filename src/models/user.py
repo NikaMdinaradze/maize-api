085c868e913cef01776f12890049d051596d151c
NikaMdinaradze <mdinaradzenika04@gmail.com>
2024-05-30 10:57:38 +0400

    Feat: add user roles

--- src/models/user.py
+++ src/models/user.py
@@ -1,7 +1,23 @@
+from enum import Enum
 from uuid import UUID, uuid4
 
 from pydantic import EmailStr
-from sqlmodel import Field, SQLModel, AutoString
+from sqlmodel import AutoString, Field, SQLModel
+
+
+class RoleEnum(str, Enum):
+    """
+    Enum representing different user roles.
+
+    Attributes:
+        admin (str): Represents an administrative user with full access to the system.
+        mentor (str): Represents a mentor user with permissions to manage courses.
+        customer (str): Represents a customer user with basic access to use the services.
+    """
+
+    admin = "admin"
+    mentor = "mentor"
+    customer = "customer"
 
 
 class UserBase(SQLModel):
@@ -11,7 +27,9 @@ class UserBase(SQLModel):
     Attributes:
         email (EmailStr): The email address of the user.
     """
+
     email: EmailStr = Field(max_length=50, unique=True, sa_type=AutoString)
+
 
 class UserCreate(UserBase):
     """
@@ -23,7 +41,9 @@ class UserCreate(UserBase):
     Attributes:
         password (str): The password for the new user.
     """
+
     password: str = Field(max_length=128)
+
 
 class UserView(UserBase):
     """
@@ -36,8 +56,11 @@ class UserView(UserBase):
         id (UUID): The unique identifier for the user.
         is_active (bool): Flag indicating if the user account is active.
     """
+
     id: UUID = Field(default_factory=uuid4, primary_key=True, index=True)
+    role: RoleEnum = RoleEnum.customer
     is_active: bool = Field(default=False)
+
 
 class User(UserCreate, UserView, table=True):
     """
@@ -47,15 +70,18 @@ class User(UserCreate, UserView, table=True):
         UserCreate: Model for creating a new user.
         UserView: Model for viewing user details.
     """
+
     pass
+
 
 class UserUpdate(SQLModel):
     """
-     Model for updating (patch) user details.
+    Model for updating (patch) user details.
 
-     Attributes:
-         email (EmailStr | None): The updated email address of the user.
-         password (str | None): The updated password of the user.
-     """
+    Attributes:
+        email (EmailStr | None): The updated email address of the user.
+        password (str | None): The updated password of the user.
+    """
+
     email: EmailStr | None = None
     password: str | None = None
