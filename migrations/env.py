b16892f0b12f9af2339a3d5fbba6cbeab3c810c1
NikaMdinaradze <mdinaradzenika04@gmail.com>
2024-06-04 19:44:51 +0400

    fix: migrations

--- migrations/env.py
+++ migrations/env.py
@@ -1,11 +1,14 @@
 import asyncio
 from logging.config import fileConfig
 
-from sqlalchemy import pool
+from sqlalchemy import pool, engine_from_config
 from sqlalchemy.engine import Connection
-from sqlalchemy.ext.asyncio import async_engine_from_config
+from sqlalchemy.ext.asyncio import AsyncEngine
+from sqlmodel import SQLModel
 
 from alembic import context
+
+from src.settings import POSTGRES_URL
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -20,7 +23,16 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = SQLModel.metadata
+
+target_metadata.naming_convention = {
+   "ix": "ix_%(column_0_label)s",
+   "uq": "uq_%(table_name)s_%(column_0_name)s",
+   "ck": "ck_%(table_name)s_%(constraint_name)s",
+   "fk": "fk_%(table_name)s_%(column_0_name)"
+         "s_%(referred_table_name)s",
+   "pk": "pk_%(table_name)s"
+}
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -65,10 +77,16 @@ async def run_async_migrations() -> None:
 
     """
 
-    connectable = async_engine_from_config(
-        config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
+    config_section = config.get_section(config.config_ini_section)
+    config_section["sqlalchemy.url"] = POSTGRES_URL
+
+    connectable = AsyncEngine(
+        engine_from_config(
+            config_section,
+            prefix="sqlalchemy.",
+            poolclass=pool.NullPool,
+            future=True,
+        )
     )
 
     async with connectable.connect() as connection:
