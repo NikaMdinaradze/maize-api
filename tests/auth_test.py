7008142bc361ad2414182f9c20be29d71e009e18
NikaMdinaradze <mdinaradzenika04@gmail.com>
2024-06-11 23:06:40 +0400

    fix: auth test with mocking sending mail

--- tests/auth_test.py
+++ tests/auth_test.py
@@ -1,15 +1,16 @@
+from unittest.mock import MagicMock, patch
+
 from httpx import AsyncClient
 
 
 async def test_register(client: AsyncClient) -> None:
-    payload = {
-        "email": "user@example.com",
-        "password": "string"
-    }
-    response = await client.post('/auth/register', json=payload)
-    response_data = response.json()
-    assert response.status_code == 200
-    assert response_data["email"] == payload['email']
-    assert "id" in response_data
-    assert response_data["role"] == "customer"
-    assert response_data["is_active"] is False
+    payload = {"email": "user@example.com", "password": "string"}
+    # mocked send mail background task
+    with patch("fastapi.BackgroundTasks.add_task", new_callable=MagicMock):
+        response = await client.post("/auth/register", json=payload)
+        response_data = response.json()
+        assert response.status_code == 200
+        assert response_data["email"] == payload["email"]
+        assert "id" in response_data
+        assert response_data["role"] == "customer"
+        assert response_data["is_active"] is False
