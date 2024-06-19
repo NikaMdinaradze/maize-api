449c2916308c8214e0b2910c53846ee6f2a54fa0
NikaMdinaradze <mdinaradzenika04@gmail.com>
2024-06-19 21:56:06 +0400

    docs: add docstring for root testing

--- tests/main_test.py
+++ tests/main_test.py
@@ -2,6 +2,9 @@ from httpx import AsyncClient
 
 
 async def test_root(client: AsyncClient) -> None:
+    """
+    Test the root URL.
+    """
     response = await client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "root url"}
