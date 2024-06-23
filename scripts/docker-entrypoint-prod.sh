ce4eafbe2c57938eb94d31b00dd64fc42779a793
NikaMdinaradze <mdinaradzenika04@gmail.com>
2024-06-23 21:24:08 +0400

    feat: add production configuration

---
+++ scripts/docker-entrypoint-prod.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -e
+
+echo "Check postgres service availability"
+python ~/check_service.py --service-name "${DB_HOST}" --ip "${DB_HOST}" --port "${DB_PORT}"
+
+echo "Apply database migrations"
+alembic upgrade head
+
+echo "Running app with uvicorn"
+uvicorn src.main:app --host 0.0.0.0 --port 80
+
+exec "$@"
